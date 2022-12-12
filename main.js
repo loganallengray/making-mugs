31df75d6f588488fa6c420657907ccaf1b8bb63c
Logan Gray <loganallengray@gmail.com>
2022-12-12 10:06:25 -0600

    finished practice

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (let i = 0; i < clay.length; i++) {
+    const mug = `${clay[i]} coffee mug`
+    toFireInKiln.push(mug);
+}
+
+console.log(toFireInKiln)
